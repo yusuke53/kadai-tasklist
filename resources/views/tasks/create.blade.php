0db2c470259a97a2af5c458be9d91f37afc56a10
yusuke.yamaki <yusuke.yamaki@rakuten.com>
2018-05-29 04:50:53 +0000

    CRUD messages

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ新規作成ページ</h1>
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+    <p>{{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
